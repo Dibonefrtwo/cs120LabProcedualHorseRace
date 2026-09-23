209e0e58bff6b7423e9670ae74f26c5fbec3e3f3
Dibonefrtwo <nanadala2@gmail.com>
2026-09-23 20:47:39 +0000

    testing refrences and pointers with arrays currently. Struggling.

--- horseRace.cpp
+++ horseRace.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
-//declare functions in here 
-
-#define bool keepGoing true
-#define int HORSE_NUM 5
-int horsePosition [5] = {};
+//declare functions in here
+void advance(int index, int* horsePosition[]);
 
 int main(){ 
- if (keepGoing == true){
-  for (int n = 0; n > HORSE_NUM; n++){ 
-   std::cout << horsePosition[n] << std::endl;
-  } // end for
- } //end if
- return 0;
+
+bool keepGoing = true; 
+const int HORSE_NUM = 5; 
+int horsePosition [5] = {};
+
+srand(time(0));
+
+for (int index = 0; index < HORSE_NUM; index++){
+ advance(index, horsePosition[index]);
+ std::cout << horsePosition[index] << std::endl;
+ } //end for
 } //end main
  
+
+void advance(int index, int* horsePosition[]){
+ int coin = rand() % 2;
+ std::cout << horsePosition[index] + coin << std::endl;
+}
